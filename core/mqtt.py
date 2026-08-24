3fe581a86a709b46b63339411c019f416c886cb1
pius-code <obliepius13@gmail.com>
2026-08-24 13:58:32 +0000

    fixed mqtt

--- core/mqtt.py
+++ core/mqtt.py
@@ -6,14 +6,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 port = os.getenv("MQTT_PORT", "8883")
-ip = os.getenv("MQTT_IP", "d1fb95ffc6654d6e98effc66d26fed74.s1.eu.hivemq.cloud") # noqa
+ip = os.getenv("MQTT_IP", "d1fb95ffc6654d6e98effc66d26fed74.s1.eu.hivemq.cloud")  # noqa
 username = os.getenv("MQTT_USER", "ashaESP")
 password = os.getenv("MQTT_PASSWORD", "ashatheworldtothefuture")
 
 client = mqtt.Client()
-client.tls_set(cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2)
+client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
 client.username_pw_set(username, password)
 
-client.connect(ip, int(port))
-client.subscribe("asha/ashaSensor/+")
-client.loop_start()
+try:
+    client.connect(ip, int(port))
+    client.subscribe("asha/ashaSensor/+")
+    client.loop_start()
+except Exception as e:
+    print(f"MQTT connection error: {e}")
